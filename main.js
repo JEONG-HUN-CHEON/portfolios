86c4941bfe36e04ed474e2c791e5f9838294e47a
JEONG-HUN-CHEON <98463804+JEONG-HUN-CHEON@users.noreply.github.com>
2022-02-21 22:09:23 +0900

    javascript; navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict'
+// Navbar 투명하게 만들기(올라가면 투명 내려가면 블루색)
+const navbar=document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log('navbarHeight: ${navbarHeight}');
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else{
+        navbar.classList.remove('navbar--dark');
+    }
+});
