a5121bccce5cc6647df23e9c3d1de05267528bd1
JEONG-HUN-CHEON <98463804+JEONG-HUN-CHEON@users.noreply.github.com>
2022-02-21 22:22:35 +0900

    javascript; ` error

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const navbar=document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
-    console.log('navbarHeight: ${navbarHeight}');
+    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else{
