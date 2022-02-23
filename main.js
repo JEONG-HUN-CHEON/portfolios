66c7cc461f71dfede5853bec99ca4185b65fdb6e
JEONG-HUN-CHEON <98463804+JEONG-HUN-CHEON@users.noreply.github.com>
2022-02-23 23:54:29 +0900

    JS; navbar IntersectionObserver

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const navbar=document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else{
@@ -104,3 +102,52 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+//1. 모든 섹션요소들을 가져온다.
+//2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+//1
+const sectionIds = [
+    '#Home', 
+    '#about', 
+    '#skills', 
+    '#work', 
+    '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+
+//2
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectedNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root : null,
+    rootMargin: '0px',
+    threshold: 0.3,
+}
+
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            //스크롤링이 아래로 되어서 페이지가 올라옴
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
